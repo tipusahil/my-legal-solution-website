6ffdf220ed733fb713bc4fcb090bc21e415d064e
tipusahil <tipusahil.ctg@gmailcom>
2025-01-11 21:15:30 +0600

    form complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//     content: ["./src/**/*.{html,js}"],
-//     theme: {
-//       extend: {},
-//     },
-//     plugins: [],
-//   }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  
+  module.exports = {
+    theme: {
+      extend: {
+        dropShadow: {
+          'lime': '0 4px 6px rgba(163, 230, 53, 0.5)', // lime-400 এর মতো শেড
+        },
+      },
+    },
+  };
+  
